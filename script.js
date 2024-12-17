a29dc03867155f48b18f4af0ec269d4d44285df2
nidhiisinghh <nidhiisinghh2007@gmail.com>
2024-12-17 15:03:39 +0530

    Update script.js
    With integrated local storage.

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ var timer;
 var display = document.querySelector(".timer-Display");
 var laps = document.querySelector(".laps");
 
+var totalSavedTimes = [];
+var lastResetTime = null;
 var clickSound = new Audio("click.mp3");
 var tickingSound = new Audio("stopwatch.mp3");
-tickingSound.loop = true; 
+tickingSound.loop = true;
 
 function playClickSound() {
     clickSound.play();
@@ -14,13 +16,12 @@ function playClickSound() {
 function start() {
     playClickSound();
     if (!timer) {
-        tickingSound.play(); 
+        tickingSound.play();
         timer = setInterval(run, 10);
     }
 }
 
 function run() {
-    display.innerHTML = getTimer();
     ms++;
     if (ms == 100) {
         ms = 0;
@@ -34,55 +35,86 @@ function run() {
         m = 0;
         h++;
     }
-    if (h == 24) {
-        h = 0;
-    }
+    display.innerHTML = getTimer();
 }
 
 function getTimer() {
-    return (h < 10 ? "0" + h : h) + " : " + (m < 10 ? "0" + m : m) + " : " + (s < 10 ? "0" + s : s) + " : " + (ms < 10 ? "0" + ms : ms);
+    return (
+        (h < 10 ? "0" + h : h) +
+        ":" +
+        (m < 10 ? "0" + m : m) +
+        ":" +
+        (s < 10 ? "0" + s : s) +
+        ":" +
+        (ms < 10 ? "0" + ms : ms)
+    );
 }
 
 function pause() {
     playClickSound();
-    stopTimer();
+    if (timer) {
+        stopTimer();
+        saveTime();
+    }
 }
 
 function stopTimer() {
     clearInterval(timer);
     timer = false;
-    tickingSound.pause(); 
-    tickingSound.currentTime = 0; 
+    tickingSound.pause();
+    tickingSound.currentTime = 0;
+}
+function restart(){
+    if(timer){
+        reset();
+        start();
+    }
+}
+function lap() {
+    let L = document.createElement("li");
+    L.innerHTML = getTimer();
+    let lapsList = document.querySelector(".laps"); 
+    lapsList.appendChild(L);
+}
+function resetLap() {
+    playClickSound();
+    totalSavedTimes = [];
+   reset();
+    laps.innerHTML = "";
+}
+
+function saveTime() {
+    let currentTime = getTimer();
+    totalSavedTimes.push(currentTime);
+    localStorage.setItem("savedTimes", JSON.stringify(totalSavedTimes));
 }
 
 function reset() {
     playClickSound();
     stopTimer();
+    lastResetTime = getTimer();
     ms = 0;
     s = 0;
     m = 0;
     h = 0;
     display.innerHTML = getTimer();
+    saveLastResetTime();
 }
 
-function restart() {
-    playClickSound();
-    if (timer) {
-        reset();
-        start();
+function saveLastResetTime() {
+    if (lastResetTime) {
+        localStorage.setItem("lastResetTime", lastResetTime);
     }
 }
 
-function lap() {
-    playClickSound();
-    if (timer) {
-        var Li = document.createElement("li");
-        Li.innerHTML = getTimer();
-        laps.appendChild(Li);
+window.onload = function () {
+    let saved = localStorage.getItem("savedTimes");
+    if (saved) {
+        totalSavedTimes = JSON.parse(saved);
     }
-}
-
-function resetLap() {
-    playClickSound();
-    laps.innerHTML = "";
-}
+    let resetSaved = localStorage.getItem("lastResetTime");
+    if (resetSaved) {
+        lastResetTime = resetSaved;
+    }
+    display.innerHTML = getTimer();
+};
