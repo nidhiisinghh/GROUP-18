08ceeab74f264c89c492bc1dbc73a1f4352bb6bd
nikhildhankhar124106 <nikhildhankhar999@gmail.com>
2024-12-17 16:51:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 var ms = 0, s = 0, m = 0, h = 0;
 var timer;
+var display = document.querySelector(".timer-Display");
+var lapsTable = document.querySelector(".laps");
 
+var totalSavedTimes = []; 
+var clickSound = new Audio("click.mp3");
+var tickingSound = new Audio("stopwatch.mp3");
+tickingSound.loop = true;
 
-var display = document.querySelector(".timer-display");
-var laps = document.querySelector(".laps ul");
-
-
+function playClickSound() {
+    clickSound.play();
+}
 
 
 function start() {
+    playClickSound();
     if (!timer) {
+        document.querySelector("#pauseTimer").innerHTML="Pause ⏸️";
+        tickingSound.play();
         timer = setInterval(run, 10);
     }
 }
+
 function run() {
-    display.innerHTML = getTimer();
     ms++;
     if (ms == 100) {
         ms = 0;
@@ -28,45 +36,75 @@ function run() {
         m = 0;
         h++;
     }
-    if (h == 24) {
-        h = 0;
-    }
+    display.innerHTML = getTimer();
 }
 
 function getTimer() {
     return (
-        (h < 10 ? "0" + h : h) + " : " +
-        (m < 10 ? "0" + m : m) + " : " +
-        (s < 10 ? "0" + s : s) + " : " +
+        (h < 10 ? "0" + h : h) +
+        ":" +
+        (m < 10 ? "0" + m : m) +
+        ":" +
+        (s < 10 ? "0" + s : s) +
+        ":" +
         (ms < 10 ? "0" + ms : ms)
     );
 }
+
 function pause() {
+    playClickSound();
+    if (timer) {
+        document.querySelector("#pauseTimer").innerHTML="Pause ⏯️";
+        stopTimer();
+        saveTime(); 
+    }
+}
+function restart(){
+    if(timer){
+        reset();
+        start();
+    }
+}
+function stopTimer() {
     clearInterval(timer);
-    timer = null;
+    timer = false;
+    tickingSound.pause();
+    tickingSound.currentTime = 0;
+}
+function lap() {
+    let L = document.createElement("li");
+    L.innerHTML = getTimer();
+    let lapsList = document.querySelector(".laps"); 
+    lapsList.appendChild(L);
+}
+
+function saveTime() {
+    let currentTime = getTimer(); 
+    totalSavedTimes.push(currentTime); 
+
+    localStorage.setItem("savedTimes",totalSavedTimes);
+
 }
 
 function reset() {
-    pause();
-    ms = s = m = h = 0;
+    document.querySelector("#pauseTimer").innerHTML="Pause";
+    playClickSound();
+    stopTimer();
+    resetLap();
+    saveTime();
+    ms = 0;
+    s = 0;
+    m = 0;
+    h = 0;
     display.innerHTML = getTimer();
 }
+ 
 
-function restart() {
-    reset();
-    start();
-}
 
-function lap() {
-    if (timer) {
-        var li = document.createElement("li");
-        li.textContent = getTimer();
-        laps.appendChild(li);
-    }
-}
 
 function resetLap() {
-    laps.innerHTML = "";
+    playClickSound();
+    totalSavedTimes = [];
+   reset();
+    lapsTable.innerHTML = "";
 }
-
-
